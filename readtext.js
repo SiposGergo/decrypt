548c4235070b853f67a796920bbfd947cba78df2
Daniel <daniel.varsas.com>
2020-10-19 15:58:14 +0200

    find # of letters

--- readtext.js
+++ readtext.js
@@ -3,4 +3,17 @@ const file = fs
     .readFileSync('./encrypted.txt', {encoding: 'utf8'})
     .toString()
 
-console.log(file);
+const count = {};
+const countChars = (encrypted, count) => {
+    for (let i = 0; i < encrypted.lenght; i++) {
+        const charAtI = encrypted[i];
+        if(count[charAtI] === undefined) {
+            count[charAtI] = 1;
+        } else {
+            count[charAtI]++;
+        }
+    }
+};
+
+countChars(file, count);
+console.log('count', count);
