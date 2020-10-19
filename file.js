50361048311c73706699f2b6b6da148622235845
hbarbi11 <hegedusbarby@gmail.com>
2020-10-19 16:01:56 +0200

    counting

--- file.js
+++ file.js
@@ -2,4 +2,20 @@ const fs = require('fs');
 const file = fs
     .readFileSync('./encrypted.txt')
     .toString();
-console.log(file);
+// console.log(file);
+
+const count = {};
+// count referencia szerint adódik át
+const countChars = (file, count) => {
+    for (let i = 0; i < file.length; i++) {
+        const charArt = file[i];
+        if(count[charArt] === undefined) {
+            count[charArt] = 1;
+        } else {
+            count[charArt]++;
+        }
+    }
+};
+
+countChars(file, count);
+console.log('count', count);
