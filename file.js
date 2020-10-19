a3add97ead502d9988ca2c452bcce83424ca2f95
pakaikarolina <pakaikarolina@gmail.com>
2020-10-19 16:28:13 +0200

    file renamed to file.js

---
+++ file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const file = fs
+.readFileSync('./encrypted.txt', {encoding: 'utf8'})
+.toString();
+console.log(file);
+
+const obj = {};
+const a = 1;
+
+const count = {};
+
+//count referencia szerint adódik át!
+const countChars = (encrypted, count) => {
+    for( let i = 0; i < encrypted.length; i++) {
+        const charAtI = encrypted[i];
+        if(count[charAtI] === undefined) {
+            count[charAtI] = 1;
+        } else {
+            count [charAtI]++;
+        }
+    }
+
+};
+countChars(file, count);
+console.log('count', count);
+
+const keys = Object.keys(count);
+console.log('keys', keys);
+const arr = [];
+for (let i = 0; i < keys.length; i++) {
+    arr.push({ char: keys[i], count: count[keys[i]]});
+}
+arr.sort((x, y) => y.count - x.count);
+console.log('arr', arr);
