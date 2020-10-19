66e46d449e316c365bfb8e615be2528bf1183234
pakaikarolina <pakaikarolina@gmail.com>
2020-10-19 15:57:22 +0200

    const countChars added

--- gyak.js
+++ gyak.js
@@ -3,3 +3,23 @@ const file = fs
 .readFileSync('./encrypted.txt', {encoding: 'utf8'})
 .toString();
 console.log(file);
+
+const obj = {};
+const a = 1;
+
+const count = {};
+
+//count referencia szerint adódik át!
+const countChars = (encrypted, count) => {
+    for( let i = 0; i < encrypted.length; i++) {
+        const charAtI = encrypted[i];
+        if(count[charAtI] === undefined) {
+            count[charAtI] = 1;
+        } else {
+            count [charAtI]++;
+        }
+    }
+
+};
+countChars(file, count);
+console.log('count', count);
