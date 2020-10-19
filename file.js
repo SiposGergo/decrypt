0e01014c19c1bce24a5e78ec9ad643431974159b
fhrmt <feher.mate.944@gmail.com>
2020-10-19 15:54:45 +0200

    karakterszám kiíratva

--- file.js
+++ file.js
@@ -2,3 +2,20 @@ const fs = require('fs');
 const file=fs.readFileSync('./encrypted.txt', { encoding: 'utf8'}).toString();
 
 console.log(file);
+
+//sortörés ascii=10
+
+const count = {};
+const countCh = (enc, count) => {
+    for (let i=0; i<enc.length; i++) {
+        const chAtI =enc[i];
+        if (count[chAtI]===undefined) {
+            count[chAtI] =1;
+        } else {
+            count[chAtI]++;
+        }
+    }
+};
+
+countCh(file, count);
+console.log('count', count);
