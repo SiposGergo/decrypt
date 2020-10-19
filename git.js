d95cf8226d0e211cee12be85897c203e02b1b4ec
nomansnick <marapatgab@gmail.com>
2020-10-19 15:55:33 +0200

    fn kesz

--- git.js
+++ git.js
@@ -1,4 +1,19 @@
 const fs = require('fs');
 const file = fs.readFileSync('./encrypted.txt', {encoding: 'utf-8'}).toString();
 
-console.log(file);
+const counter = {}
+
+const CountChar = (secrettext, count) => {
+for (let i = 0; i < secrettext.length; i++) {
+    let CharAtIndex = secrettext[i]
+    if (count[CharAtIndex] === undefined) {
+        count[CharAtIndex] = 1;
+    }
+    else {
+        count[CharAtIndex]++;
+    }
+}
+}
+
+CountChar(file, counter);
+console.log('count', counter);
