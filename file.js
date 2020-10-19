f1f09beae5577db6aeb2820be6e7937af6880d9d
Zoran Terhes <zterhes1@gmail.com>
2020-10-19 15:57:29 +0200

    added count numbers

--- file.js
+++ file.js
@@ -5,3 +5,19 @@ const file = fileStream
     .readFileSync('./encrypted.txt', { encoding: 'utf-8' })
     .toString();
 console.log(file);
+// újsor karakter ascci 10;
+
+const alphabets = {};
+//referencia szerinti értékátadás
+const count = (file, alphabets) => {
+    for (let i = 0; i < file.length; i++) {
+        if (alphabets[file[i]] === undefined) {
+            alphabets[file[i]] = 1;
+        } else {
+            alphabets[file[i]]++;
+        }
+    }
+}
+
+count(file, alphabets);
+console.log(alphabets)
