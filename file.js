95d558e7605a6be78f0b538e47023eb869dcb763
aniko <s.aniko9424@gmail.com>
2020-10-19 15:54:59 +0200

    Megszámoltuk, hogy az egyes karakterek hányszor fordulnak elő.

--- file.js
+++ file.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
 const file = fs.readFileSync('./encrypted.txt', {encoding: 'utf8'}).toString();
 
-console.log(file);
+//console.log(file);
+
+const count = {};   //ezt azért csináltuk, hogy megnézzük, hogy kint is változik az objektum, mert referencia szerint adjuk át az objektumot
+//jobb, ha a függvényen belül hozzuk létre, és visszatérünk vele
+const countChars = (encrypted, count) => {
+    for (let i = 0; i < encrypted.length; i++) {
+        const charAtI  = encrypted[i];
+        if (count[charAtI] === undefined) {
+            count[charAtI] = 1; 
+        } else {
+            count[charAtI]++;
+        }
+    }
+};
+
+countChars(file, count);
+console.log('count', count);
