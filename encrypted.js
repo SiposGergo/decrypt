c4b13251afb635ddbe7c733b3cbaa902c19e61e5
kerdotamas <kerdotamas83@gmail.com>
2020-10-19 15:59:59 +0200

    Már kiegészítve a megszámlálással

--- encrypted.js
+++ encrypted.js
@@ -4,3 +4,21 @@ const file = fs
 .toString();
 
 console.log(file)
+
+// ez már a dekódolás része, megszámoljuk a karaktereket
+
+const count = {};
+
+const countChars = (encrypted, count) => {
+    for ( let i = 0; i < encrypted.length; i++) {
+        const charAtI = encrypted[i];
+        if (count[charAtI] === undefined) {
+            count[charAtI] = 1;
+
+        } else {
+            count[charAtI]++;
+        }
+    }
+}
+countChars(file, count);
+console.log('count', count)
