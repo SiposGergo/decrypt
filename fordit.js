eeccd2396769e491a6b6800ab60e58fc5e2b8c5e
MrCroo <gabszon002@gmail.com>
2020-10-19 15:55:26 +0200

    count evrthing

--- fordit.js
+++ fordit.js
@@ -3,4 +3,19 @@ const file = fs
 .readFileSync('./encrypted.txt', {encoding: 'utf8'})
 .toString();
 
-console.log(file);
+// console.log(file);
+
+const count = {};
+const countChars = (encrypted, count ) => {
+    for(let i = 0; i < encrypted.length; i++) {
+        const charAtI = encrypted[i];
+        if(count[charAtI] === undefined) {
+            count[charAtI] = 1;
+        }else {
+            count[charAtI]++
+        }
+    }
+}
+
+countChars(file, count);
+console.log('count', count);
