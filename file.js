287cfef9d666259f957ef6b8f576408818b42a4e
fhrmt <feher.mate.944@gmail.com>
2020-10-20 15:23:51 +0200

    bubblesort added

--- file.js
+++ file.js
@@ -3,8 +3,6 @@ const file=fs.readFileSync('./encrypted.txt', { encoding: 'utf8'}).toString();
 
 console.log(file);
 
-//sortörés ascii=10
-
 const count = {};
 const countCh = (enc, count) => {
     for (let i=0; i<enc.length; i++) {
@@ -19,3 +17,25 @@ const countCh = (enc, count) => {
 
 countCh(file, count);
 console.log('count', count);
+
+const keys = Object.keys(count);
+console.log('keys', keys);
+const tomb = [];
+for (let i=0; i<keys.length; i++) {
+    tomb.push({char:keys[i], count: count[keys[i]]});
+}
+//tomb.sort((x,y) => y.count - x.count);
+const bubbleSort = (arr) => {
+    for ( let i= arr.length-1; i>0; i--) {
+        for (let j=0; j<i; j++) {
+            if (arr[j].count<arr[j+1].count) {
+                const tmp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+};
+bubbleSort(tomb);
+console.log('tomb', tomb);
+
